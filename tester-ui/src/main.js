d72b1769e09569ac8ea2cadd1c49daa1b45c794c
mengweijin <1002284406@qq.com>
2020-04-28 22:30:05 +0800

    tester-ui init

--- tester-ui/src/main.js
+++ tester-ui/src/main.js
@@ -1,15 +1,36 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import router from './router/index'
+import 'es6-promise/auto'
+import Vuex from 'vuex'
+import store from './store/index'
+import ElementUI from 'element-ui'
+import dayjs from 'dayjs'
+import axios from 'axios'
+import {get, post, patch, put, deletes } from './utils/http'
+import './assets/iconfont/iconfont.js'
 import App from './App'
-import router from './router'
+
+import 'element-ui/lib/theme-chalk/index.css'
+import './assets/style/mengweijin.css'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+Vue.use(ElementUI)
+Vue.use(Vuex)
+
+Vue.prototype.$dayjs = dayjs
+Vue.prototype.$axios = axios
+Vue.prototype.$get = get
+Vue.prototype.$post = post
+Vue.prototype.$patch = patch
+Vue.prototype.$put = put
+Vue.prototype.$delete = deletes
+
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    store,
+    render: h => h(App)
 })
--- tester-ui/src/router/index.js
+++ tester-ui/src/router/index.js
@@ -1,15 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Layout from '@/layout/index'
+import MengweijinHeader from '@/layout/mengweijinHeader'
+import Header from '@/layout/header'
+import Footer from '@/layout/footer'
+
+/**
+ * 重写路由的push方法
+ */
+const routerPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+    return routerPush.call(this, location).catch(error => error)
+}
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
+// 公共路由
+export const Routers = [{
+        path: '',
+        component: Layout,
+        redirect: '/index',
+        children: [{
+            path: '/index',
+            components: {
+                mengweijinHeader: MengweijinHeader,
+                header: Header,
+                default: () =>
+                    import ('@/views/index'),
+                footer: Footer
+            }
+        }]
+    },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+        path: '/404',
+        component: () =>
+            import ('@/views/error/404'),
     }
-  ]
+
+]
+
+export default new Router({
+    // 去掉url中的#
+    mode: 'history',
+    scrollBehavior: () => ({ y: 0 }),
+    routes: Routers
 })
